bee66cdad6771c34c372dcf5134c967f4ba92e55
Elysson Alves <elysson250102@gmail.com>
2023-09-17 22:23:00 -0300

    Materiais Complementares

---
+++ Materais complementares/Q1/src/Main.java
@@ -0,0 +1,23 @@
+/*
+Crie uma classe Java de entidade.
+Escolha uma entidade que você já propôs para seu Trabalho Prático.
+Exemplo: classe Filme (id, titulo, sinopse, diretor).
+A classe deve implementar a interface java.io.Serializable.
+Crie também uma classe que possua uma lista de objetos da entidade escolhida.
+Exemplo: classe Filmes, possuindo uma lista de Filme (List<Filme> filmes).
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
---
+++ Materais complementares/test5/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
