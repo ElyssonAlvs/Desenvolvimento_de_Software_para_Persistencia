de7fd9c2a0571b630a056e6eb411168f13fea0f2
Elysson Alves <elysson250102@gmail.com>
2023-10-23 15:50:32 -0300

    Segunda etapa - SeuCarro - SpringBoot

---
+++ Trabalho - SeuCarro/SeuCarro-1.5/SeuCarro/src/main/java/com/Spring/SeuCarro/SeuCarroApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.SeuCarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuCarroApplication.class, args);
+	}
+
+}
---
+++ Trabalho - SeuCarro/SeuCarro-1.5/SeuCarro/src/test/java/com/Spring/SeuCarro/SeuCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.SeuCarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
